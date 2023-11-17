fe4e2d60bec5860fd6628f0e9b6dfdc440859ecf
Todd Griffin <todd.griffin61@outlook.com>
2023-11-17 16:21:47 +0000

    created prisma client

---
+++ lib/prisma.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+  var prisma: PrismaClient | undefined; // This must be a `var` and not a `let / const`
+}
+
+if ( process.env.NODE_ENV === 'production' ) {
+  prisma = new PrismaClient();
+} else {
+  if ( !global.prisma ) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
