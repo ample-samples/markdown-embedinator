e54e53cb5c6a3cafbc06a9134a7c4cbc1d81b26b
Todd Griffin <todd.griffin61@outlook.com>
2023-11-17 12:51:03 +0000

    added button group and basic layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import "@uiw/react-md-editor/markdown-editor.css";
 import "@uiw/react-markdown-preview/markdown.css";
 import dynamic from "next/dynamic";
-import {Button, ButtonGroup} from "@nextui-org/react";
+import { Button, ButtonGroup } from "@nextui-org/react";
 
 
 import * as commands from "@uiw/react-md-editor/commands"
@@ -18,19 +18,20 @@ export default function Home() {
   const [value, setValue] = useState<string | undefined>('**Hello world!!!**');
 
   return (
-    <main className="flex min-h-screen flex-row items-center justify-between p-24">
-      <div className="container">
+    <main className="flex h-screen flex-row items-center justify-between p-24">
+      <div className="grid container auto-rows-min gap-8 items-center"> 
         <MDEditor
           value={value}
           onChange={setValue}
-          style={{ height: "100vh" }}
+          visiableDragbar={false}
+          height={700}
         />
+        <ButtonGroup>
+          <Button>Generate</Button>
+          <Button>Two</Button>
+          <Button>Three</Button>
+        </ButtonGroup>
       </div>
-          <ButtonGroup>
-      <Button>One</Button>
-      <Button>Two</Button>
-      <Button>Three</Button>
-    </ButtonGroup>
     </main>
   )
 }
