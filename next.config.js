65c03bcff6bec1611ed1c6bc3edce75448a66ba8
Todd Griffin <todd.griffin61@outlook.com>
2023-11-17 11:56:15 +0000

    added markdown editor and fixed type error

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+// const nextConfig = {}
 
-module.exports = nextConfig
+// next.config.js
+const removeImports = require('next-remove-imports')();
+
+// module.exports = nextConfig
+module.exports = removeImports({});
