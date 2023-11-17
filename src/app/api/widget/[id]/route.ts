a552845aa84001bd68040d515911f049f336ef4f
Todd Griffin <todd.griffin61@outlook.com>
2023-11-17 16:29:40 +0000

    set up a GET request

--- src/app/api/widget/[id]/route.ts
+++ src/app/api/widget/[id]/route.ts
@@ -1,6 +1,11 @@
+import prisma from '@/lib/prisma'
+
+async function getArticles() {
+	const articles = await prisma.article.findMany();
+	return articles;
+}
+
 export const dynamic = 'force-dynamic' // defaults to force-static
 export async function GET(request: Request, { params }: { params: { id: string } }) {
-	console.log(request)
-
-	return Response.json({ data: params.id })
+	return Response.json({ data: params.id, articles: await getArticles() });
 }
